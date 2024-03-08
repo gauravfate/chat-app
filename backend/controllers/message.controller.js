7340efd2bb5116adbb7a71c84c84e675cf500ebe
gaurav <gauravfate393@gmail.com>
2024-03-08 15:08:48 +0530

    socket io added completed the app

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+
 
 export const sendMessage = async (req, res) => {
     try {
@@ -27,16 +29,21 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         }
 
-        // SOCKET IO FUNCTIONALITY
-
-
-
         // this way it takes more time 
         // await conversation.save();
         // await newMessage.save();
 
         // this will run in parallel
         await Promise.all([conversation.save(), newMessage.save()])
+
+
+        // SOCKET IO FUNCTIONALITY
+        const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
+
 
         res.status(201).json(newMessage);
 
