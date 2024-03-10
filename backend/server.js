fae7b4e05ac8ae0b2043ba334e4eb3bb859df6ae
gaurav <gauravfate393@gmail.com>
2024-03-10 12:37:09 +0530

    This setup is for to Hoisting

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
@@ -12,6 +13,8 @@ import { app, server } from './socket/socket.js'
 // const app = express();      // we only use it once it is the entry point for express
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());    // to parse the incoming request with json payloads (from req.body)
@@ -21,11 +24,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-// app.get('/', (req, res) => {
-//     res.send('hellow world!');
-// })
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
